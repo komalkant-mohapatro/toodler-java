fb944d4a3234cf5cf0dcc62c002a4b5203f77971
Komalkant Mohapatro <trilom2023@gmail.com>
2025-11-12 18:26:21 +0000

    datatypes learnt

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,33 @@
+"use restrict"; //treat all js code as newer version.
+
+//alert(3+3)//we are using nodejs , not browser
+
+console.log(3
+    +3
+)
+ console.log("Komalkant");
+  
+ 
+ let name = "Komalkant"
+ let age = 18
+ let isLoggedIn = false
+
+
+ //DataTypes :-
+
+ //number => 2 to the power 53
+ //bigint
+ //string = " " 
+ //boolean => true/false
+ //null => standalone value
+ //undefined =>
+ //symbol => unique
+
+
+ //premitive datatype:-
+
+ //object
+
+ console.log(typeof age, typeof name, typeof isLoggedIn);
+ 
+ console.table([age,name,isLoggedIn]);
