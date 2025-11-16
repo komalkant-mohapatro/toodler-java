faec6f9ddcea77fa99c5107f5aee441d4300a3ef
Komalkant Mohapatro <trilom2023@gmail.com>
2025-11-16 18:07:53 +0000

    DataTypes-revision done

---
+++ 01_basics/datatypeesSummary.js
@@ -0,0 +1,54 @@
+// TWo Types of Data Types which is:
+
+// 1: Primitive DataType     2: Non-Primitive DataType(ReferenceType)
+
+//In Premitive DataType : There Are 7 Premitive DataTypes :
+
+// String,Boolean,Null,Undefined,Symbol,BigInt,Number
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+console.log(id === anotherId);
+const bigNumber = 34565435766543567
+
+
+//2.Non-Premitive DataType(ReferenceType):
+
+// Array, Objects, Functions
+
+
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+name: "hitesh",
+age: 22,
+}
+
+
+const myFunction = function () {
+console.log("Hello world");
+}
+console.log(typeof scoreValue);
+
+
+
+
+
+//Undefined = "undefined"
+//Null  ="object"
+// Boolean  ="boolean"
+// Number  ="number"
+// String  ="string"
+// Object (native and does not  = "object"
+// implement [[Call]])  
+// Object (native or host and does = "function"
+// implement [[Call]]
+// Object (host and does not Implementation-defined except may not be = "undefined",
+// implement [[Call]])= "boolean", "number", or "string".
