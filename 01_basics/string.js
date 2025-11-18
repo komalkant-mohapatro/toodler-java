85f83ae3c60809c304418fa72cb97c4c7a2ef2b8
Komalkant Mohapatro <trilom2023@gmail.com>
2025-11-18 19:12:06 +0000

    strings learnt

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -9,7 +9,7 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 
 
-const gameName = new String('Komalkant')
+const gameName = new String('Komalkant-Mohapatro')
 console.log(gameName [0]);
 console.log(gameName. __proto__);
 
@@ -20,9 +20,32 @@ console.log(gameName.toUpperCase());
 
 
 console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+console.log(gameName.indexOf("a"));
+
+const firstOccurance=gameName.indexOf("a")
+const secondOccurance=gameName.indexOf("a",firstOccurance+1)
+
+console.log(firstOccurance);
+console.log(secondOccurance);
+
 
 const newString = gameName.substring(0, 4)
 console.log(newString);
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
+
+
+const newStringOne = "     Komalkant     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+
+const url = "hhtps://komalkant%20mohapatro"
+
+
+console.log(url.replace('%20', '-') );
+console.log(url.includes('komalkant'));
+
+
+console.log(gameName.split('-'));
